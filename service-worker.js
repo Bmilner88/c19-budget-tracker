849458cd5c769b33e7cf124347cafe98d0b81119
Benny623 <bmilner88@gmail.com>
2022-05-31 09:52:49 -0600

    added service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const APP_PREFIX = 'BudgetTracker-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+self.addEventListener('install', function (e) {
+    const FILES_TO_CACHE = [
+        "./public/index.html",
+        "./public/css/styles.css",
+        "./public/js/index.js"
+    ];
+
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            console.log('installing cache: ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
