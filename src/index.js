4838872e07cd799d7f417cdd3f4e04ea6feace65
Benny623 <bmilner88@gmail.com>
2022-05-31 10:03:03 -0600

    got the error to go away

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 
 self.addEventListener('install', function (e) {
     const FILES_TO_CACHE = [
-        "./public/index.html",
-        "./public/css/styles.css",
-        "./public/js/index.js"
+        "../public/index.html",
+        "../public/css/styles.css",
+        "../public/js/index.js"
     ];
 
     e.waitUntil(
