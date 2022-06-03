efd8b9c300df1b58dd41f48069610e1b105c11e4
Benny623 <bmilner88@gmail.com>
2022-06-03 10:36:52 -0600

    fixed source files, added idb.js

---
+++ public/js/idb.js
@@ -0,0 +1,29 @@
+let db;
+
+const request = indexedDB.open('budget_tracker', 1);
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    if(navigator.online) {
+        //uploadTransaction();
+    }
+};
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+
+    transactionObjectStore.add(record);
+};
+
