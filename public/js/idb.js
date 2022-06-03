2ffa3624e71ed840a84eba772c3c8baa266e2f14
Benny623 <bmilner88@gmail.com>
2022-06-03 10:51:02 -0600

    finished(?) idb.js

--- public/js/idb.js
+++ public/js/idb.js
@@ -27,3 +27,38 @@ function saveRecord(record) {
     transactionObjectStore.add(record);
 };
 
+function uploadTransaction() {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+
+    const getAll = transactionObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        if(getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if(serverResponse.message) {
+                    throw new Error(serverResponse);
+                }
+                const transaction = db.transaction(['new_transaction'], 'readwrite');
+                const transactionObjectStore = transaction.objectStore('new_transaction');
+                transactionObjectStore.clear();
+                alert('All saved transactions have been submitted!');
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        };
+    };
+};
+
+window.addEventListener('online', uploadTransaction);
